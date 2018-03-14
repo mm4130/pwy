8a4492d79559da1a4b2ed9f6e207d68b57bb780a
dimitristuder <dimitristuder@gmail.com>
2018-03-14 04:53:56 -0400

    change database connection

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
 
-define("hostname","");//serveurmysql
-define("database","");
+define("hostname","ec2-54-247-81-88.eu-west-1.compute.amazonaws.com");//serveurmysql
+define("database","d9bkmcbl5j24vq");
 define("username","");
 define("password","");
+
+define("database_url", $(heroku config:get DATABASE_URL -a play-with-yourself))
 
 //define('BASE_URL', explode('index.php', $_SERVER['SCRIPT_NAME'])[0]);
 define("BASE_URL","http://localhost/pwy/");
--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,10 +1,13 @@
 <?php
 class Connexion{
 	public function connect(){
+		/*
         $dsn = 'mysql:host='.hostname.';dbname='.database.';charset=utf8';
         $ma_connexion_mysql = new PDO($dsn, username, password);
         $ma_connexion_mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $ma_connexion_mysql->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC); 
-        return $ma_connexion_mysql;
+        $ma_connexion_mysql->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+				return $ma_connexion_mysql;
+		*/
+				return pg_connect(database_url);
     }
 }
